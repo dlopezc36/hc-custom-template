436949d8bc3b8c6b26c16ad4b7bd61830b30831d
DLopez <dlopez@neu.com.co>
2023-05-10 08:40:39 -0500

    add loader

--- public/js/hc_custom_template-public.js
+++ public/js/hc_custom_template-public.js
@@ -1,20 +1,33 @@
 (function ($) {
   "use strict";
 
+  var firstLoad = true;
+
   $(document).ready(function () {
+    $(".woocommerce-checkout").append("<div id='checkout-load-curtain'><div><div class='hc-custom-loader'></div><strong>Cargando...</strong></div></div>");
+
     $(document).ajaxStop(function () {
-      updateCheckoutAccountLogin();
-      updateCheckoutCoupon();
-      updateCheckoutFreeShippingBar();
+      if (firstLoad) {
+        updateCheckoutAccountLogin();
+        updateCheckoutCoupon();
+        updateCheckoutFreeShippingBar();
+
+        $(".woocommerce-checkout #checkout-load-curtain").remove();
+
+        firstLoad = false;
+      }
     });
   });
 
   function updateCheckoutFreeShippingBar() {
-    $(".checkout-sidebar .woocommerce-shipping-totals").before("<tr><td colspan='2'><div id='free-shipping-container'></div></td></tr>");
+    $(".checkout-sidebar .woocommerce-shipping-totals").before("<tr id='free-shipping-container-row'><td colspan='2'><div id='free-shipping-container'></div></td></tr>");
+
     $("#free-shipping-container").append($(".devnet_fsl-free-shipping"));
   }
+
   function updateCheckoutCoupon() {
     $("#order_review").before("<div id='checkout-coupon'></div>");
+
     $("#checkout-coupon").append($(".checkout_coupon"));
 
     $(".woocommerce-form-coupon-toggle").remove();
@@ -28,7 +41,7 @@
     $("#checkout-login-container #username").prop("placeholder", "Nombre de usuario *");
     $("#checkout-login-container #password").prop("placeholder", "Contraseña *");
 
-    $("#checkout-login-container .woocommerce-form-login p").first().html("<strong>¿Tienes usuario y contraseña? Inicia sesión</strong>");
+    $("#checkout-login-container .woocommerce-form-login p").first().html("<strong>¿Tienes usuario y contraseña? Inicia sesión.</strong>");
     $("#checkout-login-container .woocommerce-form-login__submit").insertAfter("#checkout-login-container .form-row-last");
     $("#checkout-login-container .lost_password").prev().addClass("rememberme");
 
